8dbda9e84cd60674471df5756a7ae100cdd07cd3
Constantine Kobushka <kobushkaconstantine@gmail.com>
2024-10-28 14:35:09 +0200

    added teg picture

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,15 @@
 import imgBg from '../../../public/img/not-found-page-bg.jpg';
+import imgBg2x from '../../../public/img/not-found-page-bg@2x.jpg';
 
 import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
   return (
     <div className={css.bg}>
-      <img src={imgBg} alt='Page not found' />
+      <picture>
+        <source media='(max-width: 1440px)' srcSet={`${imgBg} 1x, ${imgBg2x} 2x`} type='image/jpg' />
+        <img src={imgBg} alt='Page not found' loading='lazy' />
+      </picture>
     </div>
   );
 };
